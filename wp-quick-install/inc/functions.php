86950be1971bbc987dfcf25711c0123a2fbf89c4
Daniel Drabik <daniel.drabik94@gmail.com>
2017-04-12 00:56:07 +0200

    Added recursive copy
    + Added recursive_copy to import files you always use during wordpress theme developing (prepared functions.php etc.)

--- wp-quick-install/inc/functions.php
+++ wp-quick-install/inc/functions.php
@@ -9,3 +9,21 @@ if ( ! function_exists( '_' ) ) {
 function sanit( $str ) {
 	return addcslashes( str_replace( array( ';', "\n" ), '', $str ), '\\' );
 }
+
+// Recursive copying ~ http://php.net/manual/en/function.copy.php#91010
+// In case of importing default files
+function recurse_copy($src,$dst) { 
+    $dir = opendir($src); 
+    @mkdir($dst); 
+    while(false !== ( $file = readdir($dir)) ) { 
+        if (( $file != '.' ) && ( $file != '..' )) { 
+            if ( is_dir($src . '/' . $file) ) { 
+                recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+            else { 
+                copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+        } 
+    } 
+    closedir($dir); 
+} 
