56f2142e8d35d4af2cc3912b9163ebca57cb45b0
Daniel Drabik <daniel.drabik94@gmail.com>
2017-04-18 11:20:04 +0200

    Update settings.php

--- wp-quick-install/import/tf/settings.php
+++ wp-quick-install/import/tf/settings.php
@@ -1,1 +1,41 @@
+<?php
+	
+	add_action( 'tf_create_options', 'custom_options' );
 
+	function custom_options() {
+		$titan = TitanFramework::getInstance( 'themename' );
+
+		header_($titan);
+		footer_($titan);
+
+	}
+
+	function header_($titan) {
+
+		$prefix = __FUNCTION__;
+
+		$panel = $titan->createAdminPanel( array(
+		'name' => 'Header',
+		'parent' => 'themes.php',
+		) );
+
+		$panel->createOption( array(
+			'type' => 'save',
+			) );
+		
+	}
+
+	function footer_($titan) {
+
+		$prefix = __FUNCTION__;
+
+		$panel = $titan->createAdminPanel( array(
+		'name' => 'Footer',
+		'parent' => 'themes.php',
+		) );
+
+		$panel->createOption( array(
+			'type' => 'save',
+			) );
+		
+	}
